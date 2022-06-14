946092b27aafad4cde2af63e036e094239f86e27
coltonklable <colton.klable@travelport.com>
2022-06-14 10:44:43 -0600

    cleaning up again

---
+++ Test Suites/CAPI XMLSelect E2E.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CAPI XMLSelect E2E</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>priya.vadakasi@travelport.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>533b199f-70ee-45e6-b1f4-75ef486875ec</testSuiteGuid>
+   <testCaseLink>
+      <guid>8d12acd3-5ccd-4a14-837b-3f63c1df6d3a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CAPI XMLSelect/CAPI XMLSelect Air E2E Flow_1G</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/test1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>priya.vadakasi@travelport.com;</mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>68107a5c-648e-4441-8986-2e0e3c046c4b</testSuiteGuid>
+   <testCaseLink>
+      <guid>74338c8e-2b60-45b3-9fc8-67515b6f0c72</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/CAPI XMLPRO/CAPI XMLPRO_E2E AirFlow</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3b18e85e-adf4-40d4-b399-126c36be91a1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Customer Scenarios/HIS</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
